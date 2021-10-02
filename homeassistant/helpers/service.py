92a8ddb8327c14d1658dba49946b8b99ba7c2a6b
Martin Hjelmare <marhje52@gmail.com>
2021-10-02 18:41:42 +0200

    Add service integration and platform

--- homeassistant/helpers/service.py
+++ homeassistant/helpers/service.py
@@ -51,6 +51,7 @@ from homeassistant.util.yaml.loader import JSON_TYPE
 if TYPE_CHECKING:
     from homeassistant.helpers.entity import Entity
     from homeassistant.helpers.entity_platform import EntityPlatform
+    from homeassistant.helpers.service_platform import PlatformService, ServicePlatform
 
 
 CONF_SERVICE_ENTITY_ID = "entity_id"
@@ -406,7 +407,7 @@ async def async_extract_config_entry_ids(
     return config_entry_ids
 
 
-def _load_services_file(hass: HomeAssistant, integration: Integration) -> JSON_TYPE:
+def load_services_file(hass: HomeAssistant, integration: Integration) -> JSON_TYPE:
     """Load services file for an integration."""
     try:
         return load_yaml(str(integration.file_path / "services.yaml"))
@@ -426,7 +427,7 @@ def _load_services_files(
     hass: HomeAssistant, integrations: Iterable[Integration]
 ) -> list[JSON_TYPE]:
     """Load service files for multiple intergrations."""
-    return [_load_services_file(hass, integration) for integration in integrations]
+    return [load_services_file(hass, integration) for integration in integrations]
 
 
 @bind_hass
@@ -699,6 +700,58 @@ async def _handle_entity_call(
             "Service %s for %s incorrectly returns a coroutine object. Await result instead in service handler. Report bug to integration author",
             func,
             entity.entity_id,
+        )
+        await result  # type: ignore
+
+
+async def integration_service_call(
+    hass: HomeAssistant,
+    platforms: Iterable[ServicePlatform],
+    call: ServiceCall,
+) -> None:
+    """Handle an integration service call.
+
+    Calls all platforms simultaneously.
+    """
+    # A list with platform services to call the service on.
+    services: list[PlatformService] = [
+        service for platform in platforms for service in platform.services.values()
+    ]
+
+    if not services:
+        return
+
+    done, pending = await asyncio.wait(
+        [
+            asyncio.create_task(
+                service.async_request_call(_handle_service_call(hass, service, call))
+            )
+            for service in services
+        ]
+    )
+    assert not pending
+    for future in done:
+        future.result()  # pop exception if have
+
+
+async def _handle_service_call(
+    hass: HomeAssistant,
+    service: PlatformService,
+    service_call: ServiceCall,
+) -> None:
+    """Handle calling service method."""
+    result = hass.async_run_job(service.async_handle_service, service_call)
+
+    # Guard because callback functions do not return a task when passed to async_run_job.
+    if result is not None:
+        await result
+
+    if asyncio.iscoroutine(result):
+        _LOGGER.error(
+            "Service %s for %s incorrectly returns a coroutine object. "
+            "Await result instead in service handler. Report bug to integration author",
+            service.async_handle_service,
+            service.service_name,
         )
         await result  # type: ignore
 
