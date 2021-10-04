354f036c77c2e779f0f1995edfae01bee8d52275
Martin Hjelmare <marhje52@gmail.com>
2021-10-05 00:28:37 +0200

    Test test_platform_error_slow_setup

--- tests/helpers/test_service_platform.py
+++ tests/helpers/test_service_platform.py
@@ -1,9 +1,15 @@
 """Test the service platform helper."""
+import asyncio
 import logging
 from unittest.mock import AsyncMock, patch
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.service_integration import ServiceIntegration
-from homeassistant.helpers.service_platform import SLOW_SETUP_WARNING
+from homeassistant.helpers.service_platform import (
+    SLOW_SETUP_WARNING,
+    AddServicesCallback,
+)
 
 from tests.common import (
     MockConfigEntry,
@@ -44,3 +50,37 @@ async def test_platform_warn_slow_setup(hass):
         assert timeout == SLOW_SETUP_WARNING
         assert logger_method == _LOGGER.warning
         assert mock_call().cancel.called
+
+
+async def test_platform_error_slow_setup(hass, caplog):
+    """Don't block startup more than SLOW_SETUP_MAX_WAIT."""
+    called = []
+
+    async def mock_setup_entry(
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        async_add_services: AddServicesCallback,
+    ) -> None:
+        called.append(1)
+        await asyncio.sleep(1)
+
+    mock_integration(hass, MockModule(ENTRY_DOMAIN))
+    mock_platform(
+        hass,
+        f"{ENTRY_DOMAIN}.{DOMAIN}",
+        MockPlatform(async_setup_entry=mock_setup_entry),
+    )
+    entry = MockConfigEntry(domain=ENTRY_DOMAIN)
+    service_integration = ServiceIntegration(hass, _LOGGER, DOMAIN, {DOMAIN: None})
+
+    with patch("homeassistant.helpers.service_platform.SLOW_SETUP_MAX_WAIT", 0):
+        await service_integration.async_setup()
+        assert not await service_integration.async_setup_entry(entry)
+        await hass.async_block_till_done()
+
+    assert len(called) == 1
+    assert f"{DOMAIN}.{ENTRY_DOMAIN}" not in hass.config.components
+    assert (
+        f"Setup of platform {ENTRY_DOMAIN} is taking longer than 0 seconds"
+        in caplog.text
+    )
